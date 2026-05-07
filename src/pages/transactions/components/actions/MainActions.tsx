822b3578f1f28ab4f45ac89bd6fb26015bcdbe41
z.baichorova <zukhrab29@gmail.com>
2026-05-07 18:48:57 +0300

    mui installed; Modal updated

--- src/pages/transactions/components/actions/MainActions.tsx
+++ src/pages/transactions/components/actions/MainActions.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import { Button } from "../../../../shared/components/ui/Button/Button"
-import { Modal } from "../../../../shared/components/ui/Modal/Modal"
+import { UModal } from "../../../../shared/components/ui/Modal/Modal"
+
+type ModalType = 'balance' | 'categories' | 'converter' | null
+
 
 export const MainActions = () => {
+    const [activeModal, setActiveModal] = useState<ModalType>(null)
+
+    const openModal = (modal: ModalType) => {
+        setActiveModal(modal)
+    }
+
+    const closeModal = () => {
+        setActiveModal(null)
+    }
+
     return (
         <div className='transactions-actions'>
-            <Button label='баланс и счета' onClick={() => { }} />
-            <Button label='категории' onClick={() => { }} />
-            <Button icon='currency_exchange' aria-label="конвертер" onClick={() => { }} />
+            <Button label='баланс и счета' onClick={() => openModal('balance')} />
+            <Button label='категории' onClick={() => openModal('categories')} />
+            <Button icon='currency_exchange' aria-label="конвертер" onClick={() => openModal('converter')} />
             <Button icon='bar_chart' aria-label="аналитика" onClick={() => { }} />
 
-            <Modal opened={false} />
+            <UModal open={activeModal === 'balance'} handleClose={closeModal} title="Balance" />
+            <UModal open={activeModal === 'categories'} handleClose={closeModal} title="Categories" />
+            <UModal open={activeModal === 'converter'} handleClose={closeModal} title="Converter" />
         </div>
     )
 }
--- src/shared/components/ui/Modal/Modal.tsx
+++ src/shared/components/ui/Modal/Modal.tsx
@@ -1,29 +1,29 @@
-import { useState } from 'react';
-import { Button } from '../Button/Button';
 import './modal.css'
+import Modal from '@mui/material/Modal';
 
 interface ModalProps {
-    opened: boolean;
+    open: boolean;
     title?: string;
     content?: string
     children?: React.ReactNode;
-    onClose?: () => void;
+    handleClose: () => void;
 }
 
-export const Modal = ({ opened, title, content, children }: ModalProps) => {
-
-    const [closed, setClosed] = useState(false);
+export const UModal = ({ open, title, content, children, handleClose }: ModalProps) => {
 
     return (
-        <div className={`modal ${opened && 'modal--opened'}`}>
-            <div className='modal__overlay'></div>
-            <div className='modal__wrapper'>
-                {title && <h3>{title}</h3>}
-                <Button icon='close' onClick={() => setClosed(!closed)} />
-                <div>
-                    {content || children}
+        <div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <div className='u-modal'>
+                    <h3>{title}</h3>
+                    <div>{content || children}</div>
                 </div>
-            </div>
+            </Modal>
         </div>
     )
 }
